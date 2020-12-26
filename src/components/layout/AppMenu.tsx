3b5ff0c84c3030fc4b22eb739f12dacb73e55550
Agustinus Nathaniel <berserk.nate@gmail.com>
2020-12-26 22:59:49 +0700

    feat: add AppMenu

---
+++ src/components/layout/AppMenu.tsx
@@ -0,0 +1,104 @@
+import {
+  Box,
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  Flex,
+  Heading,
+  IconButton,
+  Image,
+  Link,
+  Text,
+  useColorMode,
+  useDisclosure,
+  useMediaQuery,
+} from "@chakra-ui/react";
+import { BiMenu } from "react-icons/bi";
+
+type AppsType = {
+  name: string;
+  description?: string;
+  icon: string;
+  url: string;
+};
+
+const apps: AppsType[] = [
+  {
+    name: "Covid-19 Data",
+    description: "Monitor global covid-19 stats",
+    icon: "https://sznm.dev/app_icons/covid-19-data.svg",
+    url: "https://covid19.sznm.dev",
+  },
+  {
+    name: "Add to Calendar",
+    description: "Add to Calendar link / button generator.",
+    icon: "https://sznm.dev/app_icons/add-to-calendar.svg",
+    url: "https://addtocal.sznm.dev",
+  },
+  {
+    name: "InstaDLD",
+    description: "Instagram Post downloader. Support multipost download",
+    icon: "https://sznm.dev/app_icons/instadld.svg",
+    url: "https://instadld.sznm.dev",
+  },
+];
+
+const AppMenu = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { colorMode } = useColorMode();
+
+  const [isBiggerThanMobile] = useMediaQuery("(min-width: 480px)");
+
+  return (
+    <>
+      <IconButton
+        marginLeft={2}
+        aria-label="app-menu"
+        icon={<BiMenu />}
+        background="none"
+        onClick={onOpen}
+      />
+
+      <Drawer
+        placement={isBiggerThanMobile ? "right" : "top"}
+        isOpen={isOpen}
+        onClose={onClose}
+      >
+        <DrawerOverlay />
+
+        <DrawerContent>
+          <DrawerHeader>
+            <Heading size="xs">More from sznm.dev</Heading>
+          </DrawerHeader>
+
+          <DrawerBody>
+            {apps.map(({ name, icon, url, description }, index) => (
+              <Link key={index} href={url} _hover={{ textDecoration: "none" }}>
+                <Flex
+                  marginY={4}
+                  alignItems="center"
+                  padding={2}
+                  borderRadius={12}
+                  _hover={{
+                    backgroundColor:
+                      colorMode === "light" ? "gray.200" : "gray.600",
+                  }}
+                >
+                  <Image src={icon} width={12} />
+                  <Box marginLeft={4}>
+                    <Heading size="sm">{name}</Heading>
+                    {description && <Text fontSize="xs">{description}</Text>}
+                  </Box>
+                </Flex>
+              </Link>
+            ))}
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+};
+
+export default AppMenu;
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,18 +1,24 @@
 import { Flex, Heading, Box, Text } from "@chakra-ui/react";
 
 import AccessibleLink from "../AccessibleLink";
+import AppMenu from "./AppMenu";
 import ThemeToggle from "./ThemeToggle";
 
 const Header = () => {
   return (
     <Flex as="header" width="full" height={24} align="center">
-      <AccessibleLink href="/">
-        <Heading as="h1">Public APIs</Heading>
-        <Text>Find some public APIs for your next projects</Text>
-      </AccessibleLink>
+      <Box flexBasis="60%">
+        <AccessibleLink href="/">
+          <Heading as="h1">Public APIs</Heading>
+          <Text fontSize="sm">
+            Find some public APIs for your next projects
+          </Text>
+        </AccessibleLink>
+      </Box>
 
-      <Box marginLeft="auto">
+      <Box marginLeft="auto" textAlign="right" flexBasis="40%">
         <ThemeToggle />
+        <AppMenu />
       </Box>
     </Flex>
   );
