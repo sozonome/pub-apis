4859f052309f6b270f721c0dd1480a75ba63d32a
Agustinus Nathaniel <berserk.nate@gmail.com>
2021-03-30 01:04:16 +0700

    chore: implement GA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,34 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import "@fontsource/jost/latin.css";
 
 import Layout from "../components/layout";
+
+import { initGA, logPageView } from "../helpers/analytics";
 
 import customTheme from "../styles/customTheme";
 import "../styles/globals.css";
 
 const MyApp = ({ Component, pageProps }) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    initGA();
+    if (!router.asPath.includes("?")) {
+      logPageView();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    router.events.on("routeChangeComplete", logPageView);
+    return () => {
+      router.events.off("routeChangeComplete", logPageView);
+    };
+  }, [router.events]);
+
   return (
     <ChakraProvider theme={customTheme}>
       <Head>
