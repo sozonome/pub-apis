6e905c987e1befb15d521f55e8b53cf32f11d13a
Agustinus Nathaniel <berserk.nate@gmail.com>
2021-12-18 19:27:56 +0700

    chore: use preact for production build (#40)

--- next.config.js
+++ next.config.js
@@ -12,4 +12,16 @@ module.exports = withPWA({
     domains: ["api.producthunt.com"],
   },
   reactStrictMode: true,
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      });
+    }
+
+    return config;
+  },
 });
