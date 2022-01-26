0a4b428f197a7d01642f715b033714d531b0a1d5
Agustinus Nathaniel <berserk.nate@gmail.com>
2022-01-26 23:21:34 +0700

    chore: set custom eslint dirs next config
    https://nextjs.org/docs/basic-features/eslint#linting-custom-directories-and-files

--- next.config.js
+++ next.config.js
@@ -16,12 +16,15 @@ module.exports = withPWA({
     // Replace React with Preact only in client production build
     if (!dev && !isServer) {
       Object.assign(config.resolve.alias, {
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
       });
     }
 
     return config;
+  },
+  eslint: {
+    dirs: ["src"],
   },
 });
