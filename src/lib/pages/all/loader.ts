ab0e7cf43f15b7feb8afffe226f8009150a12949
Agustinus Nathaniel <berserk.nate@gmail.com>
2022-02-18 18:13:16 +0700

    refactor: revise all entry page loader

--- src/lib/pages/all/loader.ts
+++ src/lib/pages/all/loader.ts
@@ -5,18 +5,12 @@ import { getApiList } from "lib/services/publicapis/list";
 import type { APIListPageProps } from "./types";
 
 export const getStaticProps: GetStaticProps<APIListPageProps> = async () => {
-  try {
-    const fallbackData = await getApiList();
+  const fallbackData = await getApiList();
 
-    return {
-      props: {
-        fallbackData,
-      },
-      revalidate: 60,
-    };
-  } catch {
-    return {
-      notFound: true,
-    };
-  }
+  return {
+    props: {
+      fallbackData,
+    },
+    revalidate: 60,
+  };
 };
