593bb8df685f100a8c179db813e3fcf21a774717
Agustinus Nathaniel <berserk.nate@gmail.com>
2022-12-26 18:28:30 +0700

    chore: update packages to latest

--- next.config.js
+++ next.config.js
@@ -1,27 +1,13 @@
-const withPWA = require("next-pwa");
+const withPWA = require("next-pwa")({
+  disable: process.env.NODE_ENV === "development",
+  // to re-generate manifest.json, visit https://tomitm.github.io/appmanifest/
+  dest: "public",
+  register: true,
+});
 
 /** @type {import('next').NextConfig} */
 module.exports = withPWA({
-  pwa: {
-    disable: process.env.NODE_ENV === "development",
-    // to re-generate manifest.json, visit https://tomitm.github.io/appmanifest/
-    dest: "public",
-    register: true,
-  },
   reactStrictMode: true,
-  // temporarily disabled until preact compat for react 18 is ready - startTransition
-  // webpack: (config, { dev, isServer }) => {
-  //   // Replace React with Preact only in client production build
-  //   if (!dev && !isServer) {
-  //     Object.assign(config.resolve.alias, {
-  //       react: "preact/compat",
-  //       "react-dom/test-utils": "preact/test-utils",
-  //       "react-dom": "preact/compat",
-  //     });
-  //   }
-
-  //   return config;
-  // },
   eslint: {
     dirs: ["src"],
   },
