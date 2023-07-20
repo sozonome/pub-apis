1b5ad9cb5b685704adffe76d637d694b25ef4007
Agustinus Nathaniel <berserk.nate@gmail.com>
2023-07-21 00:28:19 +0700

    refactor: migrate all page

--- src/lib/services/publicapis/category/index.ts
+++ src/lib/services/publicapis/category/index.ts
@@ -1,12 +1,14 @@
 import { API_URL } from 'lib/services/publicapis/constants';
 import { usePublicApiSWR } from 'lib/services/publicapis/utils';
+import { buildUrl } from 'lib/utils/buildUrl';
 
 import type { CategoryResponse } from './types';
 
 const CATEGORY_RESOURCE_PATH = '/categories';
 
 export const getCategoryList = async () => {
-  const res = await fetch(`${API_URL}${CATEGORY_RESOURCE_PATH}`, {
+  const url = buildUrl({ baseURL: API_URL, pathname: CATEGORY_RESOURCE_PATH });
+  const res = await fetch(url, {
     next: { revalidate: 86400 },
   });
 
